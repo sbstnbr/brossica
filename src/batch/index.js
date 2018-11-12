863aad913b8eb331f4d2517c3818110bf774ad9c
Sebastien Bro <sebastien.bro.c@thalesdigital.io>
2018-11-12 10:31:54 +0000

    refactor(batch): instagram refactor

--- src/batch/index.js
+++ src/batch/index.js
@@ -1,32 +1,48 @@
-import { getData, writeFile, getRoutes } from './map';
 
-const geojson = require('geojson');
-const fs = require('fs');
+import fs from 'fs';
+import geojson from 'geojson';
+import { getData, writeFile, getRoutes } from './map';
+import { updatePostList } from './instagram';
+
+import existingPosts from '../data/posts.json';
 
 const googleSheetUrl = 'https://spreadsheets.google.com/feeds/list/1nABudmgmscjQmmZZZUUCbyC8n4HHPeEVyg-KWw6LyKQ/1/public/values?alt=json';
+const instagramURL = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=6756377666.0bf922c.05fc969e40cb4233993910dd6f12ed63';
 
-getData(googleSheetUrl)
+// getData(googleSheetUrl)
+//   .then((response) => {
+//     writeFile(`${__dirname}/../data/data.json`, JSON.stringify(response.data.feed.entry), 'Data saved!');
+//     return response.data.feed.entry;
+//   })
+//   .then((cities) => {
+//     const parsedCities = cities.map(city => ({
+//       lat: city.gsx$lat.$t.replace(/,/g, '.'),
+//       lng: city.gsx$lng.$t.replace(/,/g, '.'),
+//       to: city.gsx$to.$t,
+//       name: city.gsx$city.$t,
+//       icon: 'circle',
+//       description: `<h2>${city.gsx$city.$t}</h2><p>${city.gsx$to.$t}</p>`,
+//     }));
+//     const geoJsonCities = geojson.parse(parsedCities, { Point: ['lat', 'lng'] });
+//     writeFile(`${__dirname}/../data/cities.json`, JSON.stringify(geoJsonCities), 'Cities saved!');
+//     return cities;
+//   })
+//   .then(cities => getRoutes(cities))
+//   .then(routesResult => fs.writeFile(`${__dirname}/../data/routes.json`,
+//     JSON.stringify(routesResult), (err) => {
+//       if (err) throw err;
+//       console.log('Routes saved!');
+//     }))
+//   .catch(err => console.log('ERROR', err));
+
+getData(instagramURL)
   .then((response) => {
-    writeFile(`${__dirname}/../data/data.json`, JSON.stringify(response.data.feed.entry), 'Data saved!');
-    return response.data.feed.entry;
+    console.log(`Existing posts: ${existingPosts.length} / Updated posts: ${response.data.data.length}`);
+    return response.data.data;
   })
-  .then((cities) => {
-    const parsedCities = cities.map(city => ({
-      lat: city.gsx$lat.$t.replace(/,/g, '.'),
-      lng: city.gsx$lng.$t.replace(/,/g, '.'),
-      to: city.gsx$to.$t,
-      name: city.gsx$city.$t,
-      icon: 'circle',
-      description: `<h2>${city.gsx$city.$t}</h2><p>${city.gsx$to.$t}</p>`,
-    }));
-    const geoJsonCities = geojson.parse(parsedCities, { Point: ['lat', 'lng'] });
-    writeFile(`${__dirname}/../data/cities.json`, JSON.stringify(geoJsonCities), 'Cities saved!');
-    return cities;
-  })
-  .then(cities => getRoutes(cities))
-  .then(routesResult => fs.writeFile(`${__dirname}/../data/routes.json`,
-    JSON.stringify(routesResult), (err) => {
-      if (err) throw err;
-      console.log('Routes saved!');
-    }))
-  .catch(err => console.log('ERROR', err));
+  .then(data => updatePostList(existingPosts, data))
+  .then(posts => fs.writeFile(`${__dirname}/../data/posts.json`, JSON.stringify(posts), (err) => {
+    if (err) throw err;
+    console.log('Posts saved');
+  }))
+  .catch(err => console.error(err));
--- src/batch/instagram.js
+++ src/batch/instagram.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
-import fs from 'fs';
-import existingPosts from '../data/posts.json';
 
 const instagramURL = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=6756377666.0bf922c.05fc969e40cb4233993910dd6f12ed63';
 
@@ -12,18 +10,6 @@ async function getData(url) {
 const updatePostList = (existingList, newList) => existingList
   .concat(newList)
   .filter((post, index, self) => self.map(p => p.caption.id).indexOf(post.caption.id) === index);
-
-// getData(instagramURL)
-//   .then((data) => {
-//     console.log(`Existing posts: ${existingPosts.length} / Updated posts: ${data.length}`);
-//     return data;
-//   })
-//   .then(data => updatePostList(existingPosts, data))
-//   .then(posts => fs.writeFile(`${__dirname}/../data/posts.json`, JSON.stringify(posts), (err) => {
-//     if (err) throw err;
-//     console.log('Posts saved');
-//   }))
-//   .catch(err => console.error(err));
 
 export {
   getData, updatePostList,
